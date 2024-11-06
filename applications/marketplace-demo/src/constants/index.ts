202ee02f76ea2fed8b46d1c8b5a2c9b70a7ce770
privatejava <privatejava@yahoo.com>
2024-11-06 16:02:26 +0545

    updated to use fallback

--- applications/marketplace-demo/src/constants/index.ts
+++ applications/marketplace-demo/src/constants/index.ts
@@ -1,4 +1,5 @@
 export const API_ENDPOINT = process.env.NEXT_PUBLIC_GRAPHQL_API_ENDPOINT as string
+export const FALLBACK_ENDPOINT = process.env.NEXT_PUBLIC_GRAPHQL_FALLBACK_ENDPOINT as string
 export const BLOCKFROST_API_KEY = process.env.NEXT_PUBLIC_BLOCKFROST_API_KEY as string
 export const IPFS_GATEWAY_URL = process.env.NEXT_PUBLIC_IPFS_GATEWAY_URL as string
 export const TOKEN_LABEL = 'tAda'
--- applications/marketplace-demo/src/services/apollo-client.ts
+++ applications/marketplace-demo/src/services/apollo-client.ts
@@ -1,13 +1,30 @@
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
-import { API_ENDPOINT } from '@/constants'
+import { API_ENDPOINT, FALLBACK_ENDPOINT } from '@/constants'
+// import { onError } from '@apollo/client/link/error';
 
 const cache = new InMemoryCache()
 const link = new HttpLink({
   uri: API_ENDPOINT,
 })
 
+const errorLink = onError(({ graphQLErrors, networkError, operation, forward }) => {
+  if (networkError ) {
+      // Log the error or perform any action needed
+      console.error('Network error:', networkError);
+
+      // Modify the operation to use the fallback endpoint
+      console.log("Changing Endpoint with fallback")
+      operation.setContext({
+          uri: FALLBACK_ENDPOINT || API_ENDPOINT,
+      });
+
+      // Retry the request with the modified operation
+      return forward(operation);
+  }
+});
+
 const client = new ApolloClient({
-  link,
+  link: errorLink.concat(link),
   cache,
 })
 
